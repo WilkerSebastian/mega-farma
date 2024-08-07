f0185c2c8dcd025f3ff7fbe05a7e4ec916843ee5
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-08-07 03:57:02 -0400

    fix: fixing the option for all categories when the screen is small

--- client/src/components/layouts/header/Navbar.vue
+++ client/src/components/layouts/header/Navbar.vue
@@ -1,27 +1,25 @@
 <template>
 
-    <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle blue-mega d-flex" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="material-symbols-outlined ">
-                                Menu
-                            </span>
-                            Todas as categorias
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li v-for="categoria in categorias" :key="categoria.name" class="nav-item">
-                                <router-link class="nav-link active" to="{{ categoria.url }}"> {{ categoria.name }} </router-link>
-                            </li>
-                        </ul>
-                    </li>
-                    <li v-for="categoria in categorias" :key="categoria.name" class="nav-item">
-                        <router-link class="nav-link active" to="{{ categoria.url }}"> {{ categoria.name }} </router-link>
-                    </li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand bg-white">
+        <div class="container-lg">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle blue-mega d-flex border-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true"type="button"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
+                        <span class="material-symbols-outlined ">
+                            Menu
+                        </span>
+                        Todas as categorias
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li v-for="categoria in categorias" :key="categoria.name" class="nav-item">
+                            <router-link class="nav-link active" to="{{ categoria.url }}"> {{ categoria.name }} </router-link>
+                        </li>
+                    </ul>
+                </li>
+                <li v-for="categoria in categorias" :key="categoria.name" class="nav-item d-none d-lg-block">
+                    <router-link class="nav-link active" to="{{ categoria.url }}"> {{ categoria.name }} </router-link>
+                </li>
+            </ul>
         </div>
     </nav>
 
