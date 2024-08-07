30b66670ccd1b624ab0474a1229e2f07485b6ff5
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-08-07 02:24:50 -0400

    add: warning of no registered products

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>Welcome to the home page!</p>
-  </div>
+    <h1 class="f-k2d text-center pt-5"> Sem produtos cadastrados </h1>
 </template>
 
 <script lang="ts">
+import "@/assets/styles/fonts.css"
+
 export default {
   name: 'Home'
 };
